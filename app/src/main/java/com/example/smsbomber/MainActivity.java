1c388a4d49f8b19e6e0cd8d3eaa1b5aac8152bc5
Nayeem <nimuzzamanj@gmail.com>
2022-02-07 16:40:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/smsbomber/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.smsbomber;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText text1,text2;
+    private Button bt1;
+    private TextView tview;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        text1=findViewById(R.id.n1);
+        text2=findViewById(R.id.n2);
+        bt1=findViewById(R.id.b1);
+        tview=findViewById(R.id.tv);
+        bt1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        try {
+
+            if (v.getId()==R.id.b1)
+            {
+
+//                Bombing bombing=new Bombing();
+//                bombing.Bmb();
+//                int rpcode = bombing.status;
+//                tview.setText(String.valueOf(rpcode));
+
+            }
+        }catch (Exception e) {
+            tview.setText("Error");
+        }
+
+
+    }
+}
