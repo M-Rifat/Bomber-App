9d14f5a16809839ee79ab83406264bb222fbd2cf
Nayeem <nimuzzamanj@gmail.com>
2022-02-07 21:32:07 +0600

    amount can run now

--- app/src/main/java/com/example/smsbomber/MainActivity.java
+++ app/src/main/java/com/example/smsbomber/MainActivity.java
@@ -86,8 +86,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         queue.add(stringRequest);
     }
     private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
