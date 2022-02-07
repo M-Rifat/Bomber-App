90aa3f6abf96d17d5ae7c8373cc2f6485920d5fa
Nayeem <nimuzzamanj@gmail.com>
2022-02-07 18:51:03 +0600

    editted

--- app/src/main/java/com/example/smsbomber/MainActivity.java
+++ app/src/main/java/com/example/smsbomber/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.smsbomber;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.smsbomber.Bombing;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +40,11 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         bt1=findViewById(R.id.b1);
         tview=findViewById(R.id.tv);
         bt1.setOnClickListener(this);
+
+        if(isNetworkAvailable()){
+            tview.setText("Connection Ok");
+        }
+
     }
 
     @Override
@@ -38,11 +54,32 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
             if (v.getId()==R.id.b1)
             {
-//  importing another class
-                Bombing bombing=new Bombing();
-                bombing.Bmb();
-                int rpcode = bombing.status;
-                tview.setText(String.valueOf(rpcode));
+                String url = "https://www.shwapno.com/WebAPI/CRMActivation/Validate?Channel=W&otpCRMrequired=false&otpeCOMrequired=true&smssndcnt=8&otpBasedLogin=false&LoyaltyProvider=&MobileNO=01912986866&countryPhoneCode=%2B88";
+                String exc = new String();
+                RequestQueue queue = Volley.newRequestQueue(this);
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // enjoy your response
+                                exc = "Successfullly";
+                                Toast.makeText(this,exc,Toast.LENGTH_LONG).show();
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // enjoy your error status
+                        exc = "not succefull";
+                    }
+                });
+
+                queue.add(stringRequest);
+                //  importing another class
+//                Bombing bombing=new Bombing();
+//                bombing.httpCall();
+//                int rpcode = bombing.status;
+                tview.setText(exc);
 
             }
         }catch (Exception e) {
@@ -50,5 +87,11 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         }
 
 
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
